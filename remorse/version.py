73e8686f36176afe07a7097610581e4e51d24fa1
Sebastian Zander <github@sebastianzander.de>
2023-06-29 19:41:01 +0200

    Add version module

---
+++ remorse/version.py
@@ -0,0 +1,7 @@
+version_major = 0
+version_minor = 1
+version_patch = 0
+version_class = 'alpha'
+
+version_string = f"{version_major}.{version_minor}.{version_patch}"
+version_string_full = f"{version_major}.{version_minor}.{version_patch}-{version_class}"
